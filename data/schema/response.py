deef03bd6d0cbae038407655ecd40674e06cccef
Liran Cohen <liran@nutrino.co>
2017-01-25 20:27:16 +0200

    food id

--- data/schema/response.py
+++ data/schema/response.py
@@ -2,6 +2,7 @@ from marshmallow import fields, Schema
 
 
 class FoodItem(Schema):
+    food_id = fields.Str(load_from='foodId', dump_to='foodId')
     measurements_v2 = fields.Dict(dump_to='foodItemMeasurementUnit', dump_only=True)
     images_v2 = fields.List(fields.Dict(), dump_to='images', dump_only=True)
     display_name = fields.Str(dump_to='displayName', dump_only=True)
