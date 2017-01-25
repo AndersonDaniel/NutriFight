12deefac157e545aebc1ac987a375490eaf4ccb8
Daniel Anderson <anderson.daniel@nutrino.co>
2017-01-25 19:16:35 +0200

    More stuff to the game

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
-from flask import Flask, request, jsonify
-import json
+from flask import Flask, jsonify, send_file
+import requests
 import os
+import StringIO
 
 app = Flask(__name__)
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
@@ -8,6 +9,11 @@ APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 @app.route('/')
 def hello():
     return "Hello, Nutrition Fighter!"
+
+@app.route('/banana')
+def banana():
+    response = requests.get('https://d3anr8px62ub97.cloudfront.net/ntr_7_19400')
+    return send_file(StringIO.StringIO(response.content), mimetype='image/jpg')
 
 if __name__ == "__main__":
     app.run()
--- static/main.js
+++ static/main.js
@@ -3,71 +3,67 @@
 var WIDTH = 1300;
 var HEIGHT = 700;
 
-var GROUND = 2 * HEIGHT / 3;
+var GROUND = 30 * HEIGHT / 31;
 var FLOORWIDTH = 5;
 
 HOR_SPEED = 300;
+
+var nutrients = [
+    ['iron', 'iron.png', 100, 0, 0xBABABA],
+    ['vitamin_c', 'orange.png', 140, 0, 0xFFB137],
+    ['vitamin_a', 'broccoli.png', 180, 0, 0x33CC33],
+    ['calcium', 'milk.png', 220, 0, 0xFFFFFF],
+    ['fibers', 'fibers.png', 260, 0, 0xFFCC66]
+]
 
 var mainState = {
     preload: function() {
         game.load.image('bird', 'assets/nutrinoman.png');
         game.load.image('pipe', 'assets/pipe.png');
         game.load.image('cloud', 'assets/cloud.jpg');
+        game.load.image('banana', '/banana');
+        game.load.image('background', 'assets/background.png');
+
+        for (var i = 0; i < nutrients.length; i++) {
+            game.load.image(nutrients[i][0], 'assets/' + nutrients[i][1]);
+        }
 
         game.time.advancedTiming = true;
     },
 
-    addCloud: function() {
-        var cloud = game.add.sprite(WIDTH, Math.random() * GROUND - 80, 'cloud');
-        game.physics.arcade.enable(cloud);
-        this.bird.bringToTop();
-        cloud.body.velocity.x = -HOR_SPEED;
-        cloud.checkWorldBounds = true;
-        cloud.outOfBoundsKill = true;
-    },
+    loadFood: function(url, x, y) {
+        var self = this;
+        var food = game.add.sprite(500, 500);
+        var bmp = game.make.bitmapData();
+        game.load.onFileComplete.add(function() {
+            console.log("finished loading");
+            bmp.load('food');
+            bmp.processPixelRGB(function(pix) {
+                if (pix.r == 255 && pix.g == 255 && pix.b == 255)
+                pix.a = 0;
+                return pix;
+            }, this);
 
-    addOnePipe: function(x, y) {
-        var pipe = game.add.sprite(x, y, 'pipe');
+            food.loadTexture(bmp);
+            self.bird.bringToTop();
+            game.physics.arcade.enable(food);
+            food.body.velocity.x = -HOR_SPEED;
+            food.scale.setTo(0.02);
+        });
 
-        this.pipes.add(pipe);
-
-        game.physics.arcade.enable(pipe);
-
-        pipe.body.velocity.x = -200;
-
-        pipe.checkWorldBounds = true;
-
-        pipe.outOfBoundsKill = true;
-    },
-
-    addRowOfPipes: function() {
-        var hole = Math.floor(Math.random() * 5) + 1;
-
-        for (var i = 0; i < HEIGHT / 60; i++) {
-            if (i != hole && i != hole + 1 && i != hole + 2) {
-                this.addOnePipe(WIDTH, i * 60 + 10);
-            }
-        }
-
-        this.score += 1;
-        this.labelScore.text = this.score;
+        console.log("starting to load");
+        game.load.image('food', url);
+        game.load.start();
     },
 
     create: function() {
-        game.stage.backgroundColor = '#71c5cf';
+        this.background = game.add.tileSprite(0, 0, WIDTH, HEIGHT, 'background');
+
         game.physics.startSystem(Phaser.Physics.ARCADE);
 
-        var graphics = game.add.graphics(0, 0);
+        this.graphics = game.add.graphics(0, 0);
 
-        graphics.beginFill(0xFF3300);
-        graphics.lineStyle(FLOORWIDTH, 0xffd900, 1);
-
-        graphics.moveTo(0, GROUND);
-        graphics.lineTo(WIDTH, GROUND);
-
-        graphics.endFill();
-
-        window.graphics = graphics;
+        window.graphics = this.graphics;
 
         this.bird = game.add.sprite(100, 245, 'bird');
 
@@ -80,7 +76,7 @@ var mainState = {
         spaceKey.onDown.add(this.jump, this);
         this.pipes = game.add.group();
 
-        this.timer = game.time.events.loop(1500, this.addCloud, this);
+        // this.timer = game.time.events.loop(1500, this.addCloud, this);
 
         this.score = 0;
         this.labelScore = game.add.text(20, 20, "0", {font: "30px Tahoma", fill: "#ffffff"});
@@ -89,6 +85,20 @@ var mainState = {
         game.physics.enable(this.floor, Phaser.ARCADE);
         this.floor.body.immovable = true;
         this.floor.body.width = this.game.world.width;
+
+        /*this.bmp = game.make.bitmapData();
+        this.bmp.load('banana');
+        this.bmp.processPixelRGB(function(pix) {
+            if (pix.r == 255 && pix.g == 255 && pix.b == 255)
+            pix.a = 0;
+            return pix;
+        }, this);*/
+
+        for (var i = 0; i < nutrients.length; i++) {
+            curr = game.add.sprite(20, nutrients[i][2], nutrients[i][0]);
+        }
+
+        this.loadFood('/banana', WIDTH, HEIGHT / 2);
     },
 
     update: function() {
@@ -96,7 +106,15 @@ var mainState = {
             this.restartGame();
         }
 
-       game.physics.arcade.collide(this.floor, this.bird);
+        game.physics.arcade.collide(this.floor, this.bird);
+
+        this.background.tilePosition.x -= 5;
+
+        for (var i = 0; i < nutrients.length; i++) {
+            this.graphics.beginFill(nutrients[i][4], 1);
+            graphics.drawRect(60, nutrients[i][2], nutrients[i][3] * 10 + 30, 25);
+            graphics.endFill();
+        }
     },
 
     jump: function() {
